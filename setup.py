d9ccb0a3266e2ba033fcd97813c9a1985d8e3586
Andreww Catellier <andrew@thisisreal.net>
2019-12-02 08:45:39 -0700

    towards #4

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='photomanip',
+      version='0.2.0',
+      description='average like, all the photos',
+      author='Andrew Catellier',
+      author_email='andrew@thisisreal.net',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'': ['*.jpg']}
+      )
